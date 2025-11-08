69442a234c9b38258c89c39e1ee01fcba8979795
prasannakumar <pprasannakumar264@gmail.com>
2025-11-08 14:40:06 +0530

    Save local changes before pulling

--- projects/telegrammsg/dist/telegrammsg/assets/env.js
+++ projects/telegrammsg/dist/telegrammsg/assets/env.js
@@ -1,5 +1,4 @@
-export const environment = {
-  production: true,
+window.env = {
   apiKey: "YOUR_FIREBASE_API_KEY",
   authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
   projectId: "YOUR_FIREBASE_PROJECT_ID",
@@ -7,5 +6,5 @@ export const environment = {
   messagingSenderId: "YOUR_FIREBASE_SENDER_ID",
   appId: "YOUR_FIREBASE_APP_ID",
   measurementId: "YOUR_FIREBASE_MEASUREMENT_ID",
-  vapidKey: "YOUR_FIREBASE_VAPID_KEY"
+  vapidKey: "YOUR_FIREBASE_VAPID_KEY"  // only if using FCM for web push
 };
