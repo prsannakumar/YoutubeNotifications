359bcc2a4f7ff12ca14a76d8716db096529ebabe
prsannakumar <prsannakumar@users.noreply.github.com>
2025-11-13 07:39:35 +0000

    deploy: 1454966718a92ba63e9d70a41a62037837907cd4

---
+++ assets/env.js
@@ -0,0 +1,11 @@
+// src/assets/env.js
+window.env = {
+  apiKey: "AIzaSyAyW3X3v0EwjlprJqe2NLTEi-i5ZWO79kU",
+  authDomain: "prasanna-notify-bot.firebaseapp.com",
+  projectId: "prasanna-notify-bot",
+  storageBucket: "prasanna-notify-bot.firebasestorage.app",
+  messagingSenderId: "1017019533161",
+  appId: "1:1017019533161:web:bab151b7bd2ae118151ff8",
+  measurementId: "G-4436P5QY4R",
+  vapidKey: "BGpVDh9ufJDEY-QroL129nu5kuJUukJ4vi5FoFqUcN8EXQJ5yQvN1Yb7JdsH7gQ_k8Q2q6aD7wC9LTd5ozJnODY" // get this from Firebase Console → Cloud Messaging → Web Push certificates
+};
